ee79fe839ccfb8c1d33bac9ba236b8c96d3e6ce6
Thibaut Dubernet <thibaut.dubernet@gmail.com>
2012-10-20 11:06:12 +0200

    convenience methods to convert from bytes to doubles and reverse

--- src/main/java/org/jcompas/model/sound/SoundUtils.java
+++ src/main/java/org/jcompas/model/sound/SoundUtils.java
@@ -19,8 +19,19 @@
  * *********************************************************************** */
 package org.jcompas.model.sound;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.sound.sampled.AudioFormat;
 
+import org.jcompas.model.JCompasGlobal;
 import org.jcompas.model.sound.Pattern;
 
 /**
@@ -39,6 +50,76 @@ public final class SoundUtils {
 
 	public static AudioFormat identifyAudioFormat( final Pattern.Golpe p ) {
 		return p.getClap().getAudioFormat();
+	}
+
+	public static double[] convertSoundToDouble(
+			final AudioFormat format,
+			final byte[] soundInBytes) {
+		final ByteBuffer buffer =
+			ByteBuffer.wrap( soundInBytes ).order(
+					format.isBigEndian() ?
+						ByteOrder.BIG_ENDIAN :
+						ByteOrder.LITTLE_ENDIAN );
+
+		final List<Double> doubles = new ArrayList<Double>();
+		final int sampleSize = format.getSampleSizeInBits();
+		while (buffer.hasRemaining()) {
+			doubles.add( getNextDouble( sampleSize , buffer ) );
+		}
+
+		double[] array = new double[doubles.size()];
+
+		int i = 0;
+		for (double d : doubles) {
+			array[ i++ ] = d;
+		}
+
+		return array;
+	}
+
+	public static byte[] convertSoundToBytes(
+			final AudioFormat format,
+			final double[] soundInDoubles) {
+		final int sampleSize = format.getSampleSizeInBits();
+		ByteBuffer buffer =
+				ByteBuffer.allocate( soundInDoubles.length * sampleSize / 8 ).order(
+					format.isBigEndian() ?
+						ByteOrder.BIG_ENDIAN :
+						ByteOrder.LITTLE_ENDIAN );
+
+		for (double d : soundInDoubles) {
+			switch (sampleSize) {
+				case 8:
+					buffer.put( (byte) d );
+					break;
+				case 16:
+					buffer.putShort( (short) d );
+					break;
+				case 32:
+					buffer.putFloat( (float) d );
+					break;
+				case 64:
+					buffer.putDouble( (double) d );
+					break;
+				default:
+					throw new IllegalArgumentException( "unhandled sample size "+sampleSize );
+			}
+		}
+
+		return buffer.array();
+	}
+
+	private static double getNextDouble(
+			final int nBits,
+			final ByteBuffer buffer) {
+		switch (nBits) {
+			case 8: return buffer.get();
+			case 16: return  buffer.getShort();
+			case 32: return  buffer.getFloat();
+			case 64: return  buffer.getDouble();
+		}
+
+		throw new IllegalArgumentException ( "Unhandled sample size "+nBits+" bits" );
 	}
 }
 
