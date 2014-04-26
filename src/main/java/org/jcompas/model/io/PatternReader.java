f3e3fcbbc02edccbd72a779ee096a03da010bf76
Thibaut Dubernet <thibaut.dubernet@gmail.com>
2014-04-26 12:32:44 +0200

    reader for the new pattern format

---
+++ src/main/java/org/jcompas/model/io/PatternReader.java
@@ -0,0 +1,143 @@
+/* *********************************************************************** *
+ * project: org.jcompas.*
+ * PatternReader.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           :                                                       *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package org.jcompas.model.io;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.jcompas.model.datamodel.ClapId;
+import org.jcompas.model.datamodel.DataModel;
+import org.jcompas.model.datamodel.EstiloId;
+import org.jcompas.model.datamodel.Pattern;
+import org.jcompas.model.datamodel.Pattern.ClapLine;
+import org.jcompas.model.datamodel.Pattern.Golpe;
+import org.jcompas.model.datamodel.PatternId;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+/**
+ * @author thibautd
+ */
+public class PatternReader {
+	private final DataModel model;
+
+	public PatternReader(
+			final DataModel model) {
+		this.model = model;
+	}
+
+	public void readFile(final String patternFile) {
+		try {
+			final Document document = new SAXBuilder().build( patternFile );
+			parseDocument(document);
+		}
+		catch (JDOMException e) {
+			throw new RuntimeException(e);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void parseDocument(final Document document) {
+		final PatternId id = parseId(document);
+		final int nRepeats = parseNRepeats(document);
+		final int duration = parseDuration(document);
+		final Set<EstiloId> estilos = parseEstilos(document);
+		final List<ClapLine> lines = parseLines(document);
+
+		model.addPattern(new Pattern(id, estilos, lines, nRepeats, duration));
+	}
+
+	private static List<ClapLine> parseLines(final Document document) {
+		final List<ClapLine> lines = new ArrayList<ClapLine>();
+
+		for ( Element line : document.getRootElement().getChildren( "line" ) ) {
+			lines.add( parseLine(line) );
+		}
+
+		return lines;
+	}
+
+	private static ClapLine parseLine(final Element line) {
+		final ClapId sound = new ClapId( line.getAttribute( "soundRefId" ).getValue() );
+		final String name = line.getAttribute( "name" ).getValue();
+
+		final List<Golpe> golpes = new ArrayList<Golpe>();
+		for ( Double pos : getTimePositionOfClaps( line.getTextTrim() ) ) {
+			golpes.add( new Golpe( pos ) );
+		}
+
+		return new ClapLine( name , sound , golpes );
+	}
+
+	private static List<Double> getTimePositionOfClaps(final String description) {
+		final char[] chars = description.toLowerCase().toCharArray();
+		
+		final List<Integer> posOfClaps = new ArrayList<Integer>();
+		int countNonWhitespace = 0;
+		for (char c : chars) {
+			if ( Character.isWhitespace( c ) ) continue;
+			if ( c == 'x' ) posOfClaps.add( countNonWhitespace );
+			countNonWhitespace++;
+		}
+
+		final List<Double> positions = new ArrayList<Double>();
+		for ( Integer p : posOfClaps ) {
+			positions.add( ((double) p) / countNonWhitespace );
+		}
+		return positions;
+	}
+
+	private static Set<EstiloId> parseEstilos(final Document document) {
+		final Set<EstiloId> estilos = new LinkedHashSet<EstiloId>();
+
+		final Element estilosElem =
+			document.getRootElement().getChild( "estilos" );
+
+		for ( Element elem : estilosElem.getChildren( "estilo" ) ) {
+			final String id = elem.getAttributeValue( "refId" );
+			estilos.add( new EstiloId( id ) );
+		}
+
+		return estilos;
+	}
+
+	private static int parseDuration(final Document document) {
+		final String d = document.getRootElement().getAttribute( "nCompas" ).getValue();
+		return Integer.parseInt( d );
+	}
+
+	private static int parseNRepeats(final Document document) {
+		final String n = document.getRootElement().getAttribute( "nRepeat" ).getValue();
+		return Integer.parseInt( n );
+	}
+
+	private static PatternId parseId(final Document document) {
+		final String id = document.getRootElement().getAttribute( "refId" ).getValue();
+		return new PatternId( id );
+	}
+
+}
