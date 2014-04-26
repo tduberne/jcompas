8c89bbf41453aaa1b7affc044b647282737cab9c
Thibaut Dubernet <thibaut.dubernet@gmail.com>
2014-04-26 13:32:21 +0200

    to load dtd from classpath

--- src/main/java/org/jcompas/model/io/JCompasIOUtils.java
+++ src/main/java/org/jcompas/model/io/JCompasIOUtils.java
@@ -19,15 +19,56 @@
  * *********************************************************************** */
 package org.jcompas.model.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.log4j.Logger;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.input.sax.XMLReaders;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 /**
  * @author thibautd
  */
 public class JCompasIOUtils {
+	private static final Logger log =
+		Logger.getLogger(JCompasIOUtils.class);
+
 	public static SAXBuilder createSaxBuilder() {
-		return new SAXBuilder( XMLReaders.DTDVALIDATING );
+		final SAXBuilder builder = new SAXBuilder( XMLReaders.DTDVALIDATING );
+		builder.setEntityResolver( new ClasspathResolver( builder.getEntityResolver() ) );
+		return builder;
+	}
+
+	private static class ClasspathResolver implements EntityResolver {
+		final EntityResolver delegate;
+
+		public ClasspathResolver( final EntityResolver delegate ) {
+			this.delegate = delegate;
+		}
+
+		public InputSource resolveEntity(
+				final String publicId,
+				final String systemId)
+					throws IOException, SAXException {
+			if ( !systemId.matches( ".*\\.dtd" ) ) {
+				return delegate.resolveEntity( publicId , systemId );
+			}
+
+			final String[] path = systemId.split( "/" );
+			final String classpath = "/dtd/"+path[ path.length - 1 ];
+			final InputStream stream = getClass().getResourceAsStream( classpath  );
+
+			if ( stream != null ) {
+				log.info( classpath+" found in classpath, using it." );
+				return new InputSource( stream );
+			}
+
+			log.warn( "could not find "+classpath+"in classpath, reverting to default behavior!" );
+			return delegate.resolveEntity( publicId , systemId );
+		}
 	}
 }
 
